0322cef0015251af43a3094716554ff759aa316b
Chinazaekpere Nzekwe <chnzekwe@noreply.acad.learn2earn.ng>
2026-04-09 13:08:54 +0000

    Add asciiArt.go

---
+++ asciiArt.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func colour(text, color string) string {
+	return color + text + "\033[0m"
+}
+
+func main() {
+	line := "Hello World"
+
+	AsciiArt(line, "\033[1;35m")
+	time.Sleep(1 * time.Second)
+}
+
+func AsciiArt(input, color string) {
+	inputFile, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println("Error reading font file:", err)
+		return
+	}
+
+	lines := strings.Split(string(inputFile), "\n")
+
+	for row := 0; row < 8; row++ {
+		lineOutput := ""
+		for _, char := range input {
+			if char < ' ' || char > '~' {
+				continue
+			}
+
+			index := row + (int(char-' ') * 9) + 1
+			if index < len(lines) {
+				lineOutput += lines[index]
+			}
+		}
+		fmt.Println(colour(lineOutput, color))
+		time.Sleep(1 * time.Second)
+	}
+}
