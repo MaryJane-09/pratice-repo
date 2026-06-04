ac4dd71200e1a815a1c9d32efdf8e11e5a49568c
chnzekwe <nzekwechinaza0613@gmail.com>
2026-06-04 23:14:11 +0100

    understand more concept about servers and connecting it to html also slowing coming into ascii-art-web

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
 )
 
+type Data struct {
+	Name    string
+	Message string
+}
+
 func pageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Method:", r.Method)
-	fmt.Println("Path:", r.URL.Path)
-	fmt.Println("Host:", r.Host)
+	// fmt.Println("Method:", r.Method)
+	// fmt.Println("Path:", r.URL.Path)
+	// fmt.Println("Host:", r.Host)
+
+	if r.Method == http.MethodPost {
+		fmt.Println("Form submitted")
+		fmt.Println(r.FormValue("input"))
+		fmt.Println(r.FormValue("banner"))
+	}
+
+	data := Data{
+		Name:    "MJ",
+		Message: "Welcome to my webpage",
+	}
 
 	switch r.URL.Path {
 	case "/":
 		tmlp, err := template.ParseFiles("templates/index.html")
-		if err == nil {
+		if err != nil {
 			http.NotFound(w, r)
+			return
 		}
-		tmlp.Execute(w, nil)
+		tmlp.Execute(w, data)
 	case "/about":
 		fmt.Fprint(w, "this webpage is about printing hello MJ")
 	case "/contact":
