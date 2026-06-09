9883d36d99a9cd8c33dd4ab309f3b01969ee7ef0
chnzekwe <nzekwechinaza0613@gmail.com>
2026-06-09 10:57:04 +0100

    corrected some errors and connect ascii web with ascii art

--- ascii-art-web/ascii.go
+++ ascii-art-web/ascii.go
@@ -1,28 +1,65 @@
 package main
 
 import (
+	"ascii-art-web/asciiart"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
-	var tmpl = template.Must(template.ParseFiles("templates/index"))
+type Data struct {
+	Result string
+}
 
-	
-
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w,r, "/ascii-art", http.StatusSeeOther)
-	
+	http.Redirect(w, r, "/ascii-art", http.StatusSeeOther)
+
 }
 
 func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 
-	input := r.FormValue("input")
-	banner := r.FormValue("banner")
+	if r.Method == http.MethodGet {
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
 
-	fmt.Println(input)
-	fmt.Println(banner)
+	if r.Method == http.MethodPost {
+		input := r.FormValue("input")
+		bannerName := r.FormValue("banner")
+
+		fmt.Println(input)
+		fmt.Println(bannerName)
+
+		banner, err := asciiart.BannerCheck("banners/" + bannerName)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		_, errs := asciiart.ValidateInput(input)
+		if errs != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		result := asciiart.GenerateArt(input, banner)
+
+		ers := tmpl.Execute(w, Data{
+			Result: result,
+		})
+		if ers != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+	}
+
 }
 
 func main() {
